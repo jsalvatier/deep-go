f17cc17b734dbd1da7b7649e61073da8d8551b14
John Salvatier <jsalvatier@gmail.com>
2016-01-12 20:29:03 -0800

    add experiment notebook and make experiment object

--- logging.lua
+++ logging.lua
@@ -1,15 +1,14 @@
 require 'io'
 
-function log(experimentName, numLayers, channelSize, batchSize, rate, 
-             rateDecay, train_cost, runningTime)
+function log(experiment)
   io.popen("curl -v " ..
-      "-F entry.1216864853='" .. experimentName .. "' " ..
-      "-F entry.1905150392='" .. numLayers .. "' " ..
-      "-F entry.1937596538='" .. channelSize .. "' " ..
-      "-F entry.1622495815='" .. batchSize .. "' " ..
-      "-F entry.481250734='" .. rate .. "' " ..
-      "-F entry.686899840='" .. rateDecay .. "' " ..
-      "-F entry.1314668940='" .. train_cost[#train_cost] .. "' " ..
-      "-F entry.150702719='" .. runningTime .. "' " ..
+      "-F entry.1216864853='" .. experiment.name .. "' " ..
+      "-F entry.1905150392='" .. experiment.numLayers .. "' " ..
+      "-F entry.1937596538='" .. experiment.channelSize .. "' " ..
+      "-F entry.1622495815='" .. experiment.batchSize .. "' " ..
+      "-F entry.481250734='" .. experiment.rate .. "' " ..
+      "-F entry.686899840='" .. experiment.rateDecay .. "' " ..
+      "-F entry.1314668940='" .. experiment.train_cost[#train_cost] .. "' " ..
+      "-F entry.150702719='" .. experiment.runningTime .. "' " ..
       "https://docs.google.com/forms/d/1_Ef8l0FistzfuQFS13SNthQfztbx-24L9AKFZvQc54s/formResponse")
 end
