49b6881c4020f2b6a41229dca79af73ded709d80
Stephanie Zolayvar <zolayvars@gmail.com>
2016-01-13 05:27:19 +0000

    make experiment notebook work

--- logging.lua
+++ logging.lua
@@ -1,6 +1,6 @@
 require 'io'
 
-function log(experiment)
+function log(experiment, train_cost, runningTime)
   io.popen("curl -v " ..
       "-F entry.1216864853='" .. experiment.name .. "' " ..
       "-F entry.1905150392='" .. experiment.numLayers .. "' " ..
@@ -8,7 +8,7 @@ function log(experiment)
       "-F entry.1622495815='" .. experiment.batchSize .. "' " ..
       "-F entry.481250734='" .. experiment.rate .. "' " ..
       "-F entry.686899840='" .. experiment.rateDecay .. "' " ..
-      "-F entry.1314668940='" .. experiment.train_cost[#train_cost] .. "' " ..
-      "-F entry.150702719='" .. experiment.runningTime .. "' " ..
+      "-F entry.1314668940='" .. train_cost[#train_cost] .. "' " ..
+      "-F entry.150702719='" .. runningTime .. "' " ..
       "https://docs.google.com/forms/d/1_Ef8l0FistzfuQFS13SNthQfztbx-24L9AKFZvQc54s/formResponse")
 end
