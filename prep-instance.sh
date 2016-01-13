758946a360480ee07efb35d9ec79c88246ecd35f
Stephanie Zolayvar <zolayvars@gmail.com>
2016-01-13 23:33:48 +0000

    Update prep-instance to work

--- prep-instance.sh
+++ prep-instance.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo mkdir ~/ebs_disk
-sudo mount /dev/xvdb ebs_disk
+sudo mount /dev/xvdb ~/ebs_disk
+rm -rf deep-go
 git clone https://github.com/jsalvatier/deep-go.git
 itorch notebook --ip='*' --port=8889 --browser=none
 
