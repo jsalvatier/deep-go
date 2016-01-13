572444d376394939b65d4570da0c730d6d406424
Stephanie Watson Zolayvar <steph@Stephanies-MacBook-Pro-2.local>
2016-01-12 16:05:02 -0800

    Basic files for running experiments

---
+++ experiment1.lua
@@ -0,0 +1,30 @@
+require 'model'
+require 'train'
+require 'optimizer'
+
+useCuda = true
+iterations = 100
+numLayers = 12
+channelSize = 64
+
+batchSize = 32 
+kernels = {5}
+strides = {1}
+channels = {2}
+
+for i = 2,numLayers do
+    table.insert(kernels, 3)
+    table.insert(strides, 1)
+    table.insert(channels, channelSize)
+end
+table.insert(channels, 1)
+
+
+model = getBasicModel(numLayers, kernels, channels)
+criterion = nn.ClassNLLCriterion()
+
+rate = .01
+rateDecay = 1e-7
+optimizer = SGD.new(rate, rateDecay)
+
+train(model, criterion, batchSize, iterations, optimzer, useCude)
