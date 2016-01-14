76a12c1a8124efd57a337db9daf070f622f46549
Stephanie Watson Zolayvar <steph@Stephanies-MacBook-Pro-2.local>
2016-01-13 16:37:05 -0800

    Pass iterations to run function.  Also move imports from notebook to experiments.lua, and make logging catch empty train_cost and validation_cost, and add iterations to logging

--- logging.lua
+++ logging.lua
@@ -1,6 +1,15 @@
 require 'io'
 
 function log(experiment, trainCost, runningTime, validationCost)
+  last_train = 'nope'
+  if #trainCost ~= 0 then
+    last_train = trainCost[#trainCost]
+  end
+  last_val = 'nope'
+  if #validationCost ~= 0 then
+    last_val = validationCost[#validationCost]
+  end
+
   io.popen("curl -v " ..
       "-F entry.1216864853='" .. experiment.name .. "' " ..
       "-F entry.1905150392='" .. experiment.numLayers .. "' " ..
@@ -8,8 +17,9 @@ function log(experiment, trainCost, runningTime, validationCost)
       "-F entry.1622495815='" .. experiment.batchSize .. "' " ..
       "-F entry.481250734='" .. experiment.rate .. "' " ..
       "-F entry.686899840='" .. experiment.rateDecay .. "' " ..
-      "-F entry.1314668940='" .. trainCost[#trainCost] .. "' " ..
+      "-F entry.1314668940='" .. last_train .. "' " ..
       "-F entry.150702719='" .. runningTime .. "' " ..
-      "-F entry.1128574508='" .. validationCost .. "' " ..
+      "-F entry.1128574508='" .. last_val .. "' " ..
+      "-F entry.814635649='" .. experiment.iterations .. "' " ..
       "https://docs.google.com/forms/d/1_Ef8l0FistzfuQFS13SNthQfztbx-24L9AKFZvQc54s/formResponse")
 end
