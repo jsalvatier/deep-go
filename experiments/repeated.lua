b43c83909bf75a836599a5b21de36fc631f49237
John Salvatier <jsalvatier@gmail.com>
2016-01-15 22:11:07 -0800

    code for repeated experiments

--- experiments/repeated.lua
+++ experiments/repeated.lua
@@ -1,6 +1,7 @@
 require 'experiments'
 require 'cutorch'
 require 'cunn'
+require 'optimizer'
 
 cmd = torch.CmdLine()
 cmd:option('-gpu', 1, "gpu to run on")
@@ -8,11 +9,14 @@ cmd:option('-num', 1, "experiment number")
 cmd:option('-iters', 100, "experiment number")
 local opt = cmd:parse(arg or {})
 
-local best_previous = '0.95223742560484.model'
-experiment = torch.load(best_previous)
-experiment.id = "repeatedRun" .. opt.num
 cutorch.setDevice(opt.gpu)
+
+local best_previous = '0.95223742560484.model'
+experiment = Experiment:load(best_previous)
+experiment.id = "repeatedRun" .. opt.num
+experiment.optimizer = SGD.new(experiment.rate, experiment.rateDecay)
+
 
 print("experiment rate", experiment.rate)
 
-experiment:run{iters=iters}
+experiment:run{iters=opt.iters}
